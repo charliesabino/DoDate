31e5fd1debbc851141decd239e7eb6f2f9676301
charliesabino <94320712+charliesabino@users.noreply.github.com>
2022-08-30 09:30:47 -0700

    once again changed container className type

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,10 +1,6 @@
 import clsx from 'clsx'
-import { ReactElement } from 'react'
 
-export const Container: React.FC<{
-  className: string
-  children: ReactElement
-}> = ({ className, children, ...props }) => {
+export const Container = ({ className, ...props }: { className: string }) => {
   return (
     <div
       className={clsx('mx-auto max-w-7xl px-4 sm:px-6 lg:px-8', className)}
