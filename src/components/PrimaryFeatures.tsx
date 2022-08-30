760ad0664cac09db6a9dd365b544df2725d39702
charliesabino <94320712+charliesabino@users.noreply.github.com>
2022-08-30 16:37:20 -0700

    any type for matches in primary featuers

--- src/components/PrimaryFeatures.tsx
+++ src/components/PrimaryFeatures.tsx
@@ -43,7 +43,7 @@ export function PrimaryFeatures() {
   useEffect(() => {
     let lgMediaQuery = window.matchMedia('(min-width: 1024px)')
 
-    function onMediaQueryChange({ matches }) {
+    function onMediaQueryChange({ matches }: any) {
       setTabOrientation(matches ? 'vertical' : 'horizontal')
     }
 
