6a281528168b435ec36815c3315594ac4d20f806
charliesabino <94320712+charliesabino@users.noreply.github.com>
2022-09-05 15:56:34 -0400

    removed features and faqs from header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -98,8 +98,8 @@ export function Header() {
               <LogoDark className='h-10 w-auto' />
             </Link>
             <div className='hidden md:flex md:gap-x-6'>
-              <NavLink href='#features'>Features</NavLink>
-              <NavLink href='#faq'>Faq</NavLink>
+              {/*<NavLink href='#features'>Features</NavLink>
+              <NavLink href='#faq'>Faq</NavLink> */}
             </div>
           </div>
           <div className='flex items-center gap-x-5 md:gap-x-8'>
