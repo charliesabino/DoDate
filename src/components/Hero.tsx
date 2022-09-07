47ee9439a14a9084fdb954db9a1fcdd193c64140
charliesabino <94320712+charliesabino@users.noreply.github.com>
2022-09-06 20:06:39 -0400

    added video button in hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -27,18 +27,19 @@ export function Hero() {
         <Button className='' href='/login'>
           Get started
         </Button>
-        {/*<Button
-          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-          variant="outline"
+        <Button
+          href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+          variant='outline'
+          className=''
         >
           <svg
-            aria-hidden="true"
-            className="h-3 w-3 flex-none fill-blue-600 group-active:fill-current"
+            aria-hidden='true'
+            className='h-3 w-3 flex-none fill-blue-600 group-active:fill-current'
           >
-            <path d="m9.997 6.91-7.583 3.447A1 1 0 0 1 1 9.447V2.553a1 1 0 0 1 1.414-.91L9.997 5.09c.782.355.782 1.465 0 1.82Z" />
+            <path d='m9.997 6.91-7.583 3.447A1 1 0 0 1 1 9.447V2.553a1 1 0 0 1 1.414-.91L9.997 5.09c.782.355.782 1.465 0 1.82Z' />
           </svg>
-          <span className="ml-3">Watch video</span>
-        </Button> */}
+          <span className='ml-3'>Watch video</span>
+        </Button>
       </div>
       <div className='mt-36 lg:mt-44'></div>
     </Container>
